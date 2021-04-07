e1dcac469204185d58f2889f22550af2895c3eee
Nacho1593 <jcarricart@gmail.com>
2021-04-07 13:37:11 -0300

    cambios Pocos

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+const mysql = require("mysql2");
 const port = 3000;
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "Blog",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("¡Nos conectamos al Blog!");
+});
 
 app.set("view engine", "ejs");
 app.listen(port, (req, res) => console.log("Server has starter ..."));
