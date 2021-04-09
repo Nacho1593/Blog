0dbd54675c730c233d321e709b8807a0b866e48b
Nacho1593 <jcarricart@gmail.com>
2021-04-09 18:13:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const formidable = require("formidable");
+const { urlencoded } = require("express");
 
 const port = 3305;
 
 app.set("view engine", "ejs");
 app.use(express.static("public/")); //permite usar mi css
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(routes);
 
