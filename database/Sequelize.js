2454bf3462bc5a5dd37ac18c8c9c1147305d0480
Nicolas Curbelo <nicolas.curbelo.sarmiento@gmail.com>
2021-04-17 16:56:40 -0300

    facebook login pronto

---
+++ database/Sequelize.js
@@ -0,0 +1,33 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const ArticleModel = require("../models/Article");
+const AuthorModel = require("../models/Authors");
+const CommentModel = require("../models/Comment");
+
+const sequelize = new Sequelize("blog3", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+  logging: false,
+});
+
+const Article = ArticleModel(sequelize, Sequelize);
+const Author = AuthorModel(sequelize, Sequelize);
+const Comment = CommentModel(sequelize, Sequelize);
+
+Article.findAllArticles = Article.findAll({
+  limit: 12,
+  order: [["createdAt", "DESC"]],
+});
+
+Article.belongsTo(Author);
+Author.hasMany(Article);
+
+Comment.belongsTo(Article);
+Article.hasMany(Comment);
+
+module.exports = {
+  sequelize,
+  Sequelize,
+  Article,
+  Author,
+  Comment,
+};
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const routes = require("./routes");
@@ -10,6 +11,88 @@ app.set("view engine", "ejs");
 app.use(express.static("public/")); //permite usar mi css
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+<<<<<<< Updated upstream
+=======
+
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { Author } = require("./database/Sequelize");
+const FacebookStrategy = require("passport-facebook");
+
+app.use(flash());
+
+app.use(
+  session({
+    secret: "supersecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    try {
+      const user = await Author.findOne({ where: { email: username } });
+      if (!user) {
+        return done(null, false, {
+          message: "Usuario incorrecto",
+        });
+      }
+      if (user.password !== password) {
+        return done(null, false, {
+          message: "Contraseña incorrecta",
+        });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  Author.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, user);
+    });
+});
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: 289628189443236,
+      clientSecret: "4e8e6d743c8c20cfdc75d6acf42a4258",
+      callbackURL: "http://localhost:3000/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      Author.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/auth/facebook/admin",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/admin");
+  }
+);
+//NOTE, ROUTES MUST BE IN THE END JUST BEFORE THE PORT-LISTEN IN ORDER TO MAKE PASSPORT WORK PROPERLLY
+>>>>>>> Stashed changes
 app.use(routes);
 
 app.listen(port, (req, res) => console.log("Server has starter ..."));
