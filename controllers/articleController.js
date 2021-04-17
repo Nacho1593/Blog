7d85c84d84f7a5826db784ebaae81c17419a3b53
Nacho1593 <jcarricart@gmail.com>
2021-04-17 14:54:13 -0300

    limpiando

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -17,39 +17,4 @@ const showArticle = async (req, res) =>
     res.render("article.ejs", { article });
   });
 
-/* async function show(req, res) {
-  const article = await Article.findById(req.params.id);
-  res.render("articulos.ejs", { article });
-} */
-
-///        /CONNECT ARTICLE CREATION
-/* User.create({
-  firstname: "María",
-  lastname: "Pérez",
-  email: "mariaperez@gmail.com",
-}).then((user) => {
-  console.log(user);
-}); */
-
-///        /CONNECT ARTICLE DESTRUCTION
-/* User.destroy({
-  where: {
-    firstname: "Pablo",
-  },
-}).then(() => {
-  console.log("¡Usuarios eliminados!");
-}); */
-
-///        /CONNECT ARTICLE UPDATE
-/* User.update(
-  { lastname: "Pérez" },
-  {
-    where: {
-      lastname: "Gómez",
-    },
-  }
-).then(() => {
-  console.log("¡Usuarios actualizados!");
-}); */
-
 module.exports = { showHome, showAdmin, showArticle };
