c5b3f8b4ce3a87164316e208bdf4da9162da76dd
Nacho1593 <jcarricart@gmail.com>
2021-04-17 15:51:25 -0300

    google

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const flash = require("express-flash");
 const app = express();
 const routes = require("./routes");
-const bcrypt = require("bcrypt");
 const compare = require("./middlewares/bcrypt");
+require("./passport-setup");
 
 const port = 3000;
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 const session = require("express-session");
 const passport = require("passport");
+const cookieSession = require("cookie-session");
 const LocalStrategy = require("passport-local").Strategy;
 const { Author } = require("./database/Sequelize");
 
@@ -62,6 +64,55 @@ passport.deserializeUser(function (id, done) {
     .catch((error) => {
       done(error, user);
     });
+});
+
+app.use(
+  cookieSession({
+    name: "tuto-session",
+    keys: ["key1", "key2"],
+  })
+);
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// Example protected and unprotected routes
+app.get("/", (req, res) => res.render("pages/index"));
+app.get("/failed", (req, res) => res.send("You Failed to log in!"));
+
+// In this route you can see that if the user is logged in u can acess his info in: req.user
+app.get("/good", isLoggedIn, (req, res) => {
+  res.render("pages/profile", {
+    name: req.user.displayName,
+    pic: req.user.photos[0].value,
+    email: req.user.emails[0].value,
+  });
+});
+
+// Auth Routes
+app.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/failed" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/good");
+  }
+);
+
+app.get("/logout", (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect("/");
 });
 
 //NOTE, ROUTES MUST BE IN THE END JUST BEFORE THE PORT-LISTEN IN ORDER TO MAKE PASSPORT WORK PROPERLLY
